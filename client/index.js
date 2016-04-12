0d77790979237dd4bf9505c0ffd834b92e403a8b
Ron Waldon <jokeyrhyme@gmail.com>
2016-04-13 09:22:12 +1000

    use updated busmq, include Node.js client code

---
+++ client/index.js
@@ -0,0 +1,63 @@
+// https://github.com/capriza/node-busmq#browser-support
+
+const Bus = require('busmq');
+
+const log = (msg) => console.log(msg);
+
+log('bus starting...');
+
+const HOSTNAME = 'docker.local';
+const HOST = `${HOSTNAME}:3000`;
+
+const bus = Bus.create({
+  // redis: [`redis://${HOSTNAME}:6379`]
+  federate: {
+    poolSize: 5,
+    urls: [`http://${HOST}/bus/federated`],
+    secret: 'mysecret'
+  }
+});
+
+bus.on('online', () => log('bus online'));
+bus.on('offline', () => log('bus offline'));
+bus.on('error', (err) => log(`bus error: ${err}`));
+
+const SUBJECT = 'heartbeat';
+const startPubSub = (pubsub) => {
+  [
+    'subscribed', 'unsubscribed'
+  ].forEach((event) => {
+    pubsub.on(event, () => log(`pubsub [${SUBJECT}]: event = ${event}`));
+  });
+  pubsub.on('message', (msg) => log(`pubsub [${SUBJECT}]: msg = ${msg}`));
+  pubsub.on('error', (err) => log(`pubsub [${SUBJECT}]: err = ${err}`));
+
+  pubsub.subscribe();
+
+  setInterval(() => {
+    pubsub.publish('ping');
+  }, 15e3);
+};
+
+const startFedPubSub = (fed) => {
+  [
+    'unauthorized', 'close', 'ready'
+  ].forEach((event) => {
+    fed.on(event, () => log(`fed: event = ${event}`));
+  });
+  fed.on('error', (err) => log(`fed: err = ${err}`));
+
+  fed.on('ready', (pubsub) => {
+    startPubSub(pubsub);
+  });
+};
+
+bus.on('online', () => {
+  const fed = bus.federate(bus.pubsub(SUBJECT), `http://${HOST}/bus/federated`);
+  startFedPubSub(fed);
+
+  // const pubsub = bus.pubsub(SUBJECT);
+  // startPubSub(pubsub);
+});
+
+bus.connect();
