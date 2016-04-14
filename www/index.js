13c08d1657d8e692e664429ef12e551da4a12847
Ron Waldon <jokeyrhyme@gmail.com>
2016-04-14 10:33:28 +1000

    polyfill `TextDecoder` to gain Safari support

--- www/index.js
+++ www/index.js
@@ -1,3 +1,8 @@
 'use strict';
 
+// poly-fill for TextEncoder
+import encoding from 'text-encoding';
+global.TextEncoder = global.TextEncoder || encoding.TextEncoder;
+global.TextDecoder = global.TextDecoder || encoding.TextDecoder;
+
 import './bus.js';
